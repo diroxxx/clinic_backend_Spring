038b9e46437084eedb718b7fcf1622e86b0cf7b6
Dominik Chełchowski <che.dom29@gmail.com>
2024-10-02 14:03:38 +0200

    Initial commit

---
+++ Przychodnia_Weterynaryjna/src/main/java/org/example/przychodnia_weterynaryjna/PrzychodniaWeterynaryjnaApplication.java
@@ -0,0 +1,13 @@
+package org.example.przychodnia_weterynaryjna;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrzychodniaWeterynaryjnaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PrzychodniaWeterynaryjnaApplication.class, args);
+    }
+
+}
---
+++ Przychodnia_Weterynaryjna/src/test/java/org/example/przychodnia_weterynaryjna/PrzychodniaWeterynaryjnaApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.przychodnia_weterynaryjna;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PrzychodniaWeterynaryjnaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
